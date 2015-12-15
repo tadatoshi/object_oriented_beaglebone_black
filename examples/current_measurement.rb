033dd02abe0ea8849769863d0f6bcd1fdc1a68da
Tadatoshi Takahashi <tadatoshi.3.takahashi@gmail.com>
2015-12-14 20:54:38 -0500

    Added missing require.

--- examples/current_measurement.rb
+++ examples/current_measurement.rb
@@ -1,6 +1,7 @@
 require 'object_oriented_beaglebone_black'
 
 require 'current_measurement/current_input'
+require 'current_measurement/current_input_with_point04_shunt'
 
 module CurrentMeasurement
   # Your code goes here...
--- examples/voltage_measurement.rb
+++ examples/voltage_measurement.rb
@@ -1,6 +1,7 @@
 require 'object_oriented_beaglebone_black'
 
 require 'voltage_measurement/voltage_input'
+require 'voltage_measurement/voltage_input_with_100k_and_20k_voltage_divider'
 
 module VoltageMeasurement
   # Your code goes here...
