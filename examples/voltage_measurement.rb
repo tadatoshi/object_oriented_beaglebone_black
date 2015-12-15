f736dde160348f3b4748d10845202bfe907a0460
Tadatoshi Takahashi <tadatoshi.3.takahashi@gmail.com>
2015-12-15 11:44:39 -0500

    Added missing 'require'.

--- examples/voltage_measurement.rb
+++ examples/voltage_measurement.rb
@@ -2,6 +2,7 @@ require 'object_oriented_beaglebone_black'
 
 require 'voltage_measurement/voltage_input'
 require 'voltage_measurement/voltage_input_with_100k_and_20k_voltage_divider'
+require 'voltage_measurement/voltage_input_with_100k_and_11k_voltage_divider'
 
 module VoltageMeasurement
   # Your code goes here...
